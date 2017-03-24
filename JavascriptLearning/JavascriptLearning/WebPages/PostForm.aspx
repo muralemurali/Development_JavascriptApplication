17a7d0edcbf7a1a5f3ce707ceb4a50258b22b94b
Muralidharan-Idhayaraja <murali.assim@gmail.com>
2017-03-24 14:08:32 +0530

    Javascript Code Updated

---
+++ JavascriptLearning/JavascriptLearning/WebPages/PostForm.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PostForm.aspx.cs" Inherits="JavascriptLearning.WebPages.PostForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <p>This is to announce that this will be used for Forking Git Hub Purpose</p>
+        <asp:Button ID="btnSubmit" runat="server" Text="Click Me" OnClick="btnSubmit_Click" />
+        <asp:Label ID="lblStatus" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ JavascriptLearning/JavascriptLearning/WebPages/PostForm.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JavascriptLearning.WebPages
+{
+    public partial class PostForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                lblStatus.Text = "Forking Documentation";
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Completed : Forking Documentation";
+        }
+    }
+}
